5f1c7c5e1fb83dba7f71738e96ea7cb933a3290b
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-02-06 18:45:17 +0100

    display documentation for components (behind additional feature)

--- crates/bevy-inspector-egui/src/bevy_inspector/mod.rs
+++ crates/bevy-inspector-egui/src/bevy_inspector/mod.rs
@@ -360,6 +360,11 @@ pub(crate) fn ui_for_entity_components(
             continue;
         }
 
+        #[cfg(feature = "documentation-for-components")]
+        let type_docs = type_registry
+            .get_type_info(component_type_id)
+            .and_then(|info| info.docs());
+
         // create a context with access to the world except for the currently viewed component
         let (mut component_view, world) = world.split_off_component((entity, component_type_id));
         let mut cx = Context {
@@ -384,7 +389,7 @@ pub(crate) fn ui_for_entity_components(
             set_highlight_style(ui);
         }
 
-        header.show(ui, |ui| {
+        let _response = header.show(ui, |ui| {
             ui.reset_style();
 
             let inspector_changed = InspectorUi::for_bevy(type_registry, &mut cx)
@@ -394,6 +399,10 @@ pub(crate) fn ui_for_entity_components(
                 set_changed();
             }
         });
+        #[cfg(feature = "documentation-for-components")]
+        if let Some(type_docs) = type_docs {
+            _response.header_response.on_hover_text(type_docs);
+        };
         ui.reset_style();
     }
 }
